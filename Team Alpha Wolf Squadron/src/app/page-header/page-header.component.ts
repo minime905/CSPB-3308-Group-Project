7e6eea1f0070637d42716b5c1e2bae529910799b
minime905 <47464412+minime905@users.noreply.github.com>
2020-02-07 21:29:20 -0700

    Login/Logout
    -Base Login and logout functioning
     -user tracking/user information ready for implementing

--- Team Alpha Wolf Squadron/src/app/page-header/page-header.component.ts
+++ Team Alpha Wolf Squadron/src/app/page-header/page-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-page-header',
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageHeaderComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public auth: AuthService) { }
+  
   ngOnInit() {
   }
 
