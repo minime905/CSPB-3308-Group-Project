973bde2841fc2925b0f0c97c536fa0d3b8cfb183
calebclough <34039275+calebclough@users.noreply.github.com>
2020-03-15 22:47:35 -0700

    Added Build Page

--- Team Alpha Wolf Squadron/src/app/app-routing.module.ts
+++ Team Alpha Wolf Squadron/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MoreInfoComponent } from './more-info/more-info.component';
+import { BuildComponent } from './build/build.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'more-info', component: MoreInfoComponent},
+  {path: 'build', component: BuildComponent},
 
   //otherwise redirect to home!!!!
   {path: '**', redirectTo: ''}
