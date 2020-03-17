83ef3b2696e7a9968dbbc34d18f05614ace2c33b
SecondEdition <maxwellfth@gmail.com>
2020-03-17 08:31:52 -0600

    Update nav bar. Various file and app cleanup. Add TESTING.md

--- Team Alpha Wolf Squadron/src/app/app-routing.module.ts
+++ Team Alpha Wolf Squadron/src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { MoreInfoComponent } from './more-info/more-info.component';
 import { BuildComponent } from './build/build.component';
+import { CompareComponent } from './compare/compare.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'more-info', component: MoreInfoComponent},
   {path: 'build', component: BuildComponent},
+  {path: 'compare', component: CompareComponent},
+
 
   //otherwise redirect to home!!!!
   {path: '**', redirectTo: ''}
--- Team Alpha Wolf Squadron/src/app/app.module.ts
+++ Team Alpha Wolf Squadron/src/app/app.module.ts
@@ -5,22 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { MoreInfoComponent } from './more-info/more-info.component';
 import { PageHeaderComponent } from './page-header/page-header.component';
 import { AdminComponent } from './admin/admin.component';
 import { BuildComponent } from './build/build.component';
+import { CompareComponent } from './compare/compare.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    LoginComponent,
-    MoreInfoComponent,
     PageHeaderComponent,
     AdminComponent,
-    BuildComponent
+    BuildComponent,
+    CompareComponent
   ],
   imports: [
     BrowserModule,
